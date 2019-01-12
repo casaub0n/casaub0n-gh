2d129c16c8223caedb3eecbac5ca820cd72b0555
Casaubon <netzach.yoshida@gmail.com>
2019-01-12 21:04:16 +0900

    repository init

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function MySkill() {
+  return <div>
+    <h2>My Skill</h2>
+      <ul>
+        <li>Java</li>
+        <li>Publish SubScribe(Redis)</li>
+        <li>ShellScript</li>
+        <li>JavaScript</li>
+        <li>Linux</li>
+      </ul>
+  </div>
+}
+
+function Header(props: { name: string }) {
+  return <h1>{props.name} Page</h1>;
+}
+
+ReactDOM.render(<Header name="Casaub0n" />, document.querySelector(".header"))
+ReactDOM.render(<MySkill />, document.querySelector(".mySkill"))
+

d75c42e282e8330a0bb506a2c05f0e565344f52e
Casaubon <netzach.yoshida@gmail.com>
2019-03-21 14:16:15 +0900

    add meta tag

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Helmet } from 'react-helmet'
 import styled, { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
@@ -22,15 +23,21 @@ const MySkill = () => {
   </>
 }
 
-const HeaderStyle = styled.header`
+const HeaderStyle = styled.h1`
   font-family: 'NotoSansJP';
   font-weight: normal;
 `
 
-const Header = (props: { name: string }) => <HeaderStyle>{props.name} Page</HeaderStyle>
+const Header = (props: { name: string }) => <header><HeaderStyle>{props.name} Page</HeaderStyle></header>
 
 const App = () => {
   return <>
+    <Helmet>
+      <meta property='og:type' content='website'/>
+      <meta property='og:description' content='Casaub0n WebSite'/>
+      <meta property='og:site_name' content='Casaub0n WebSite'/>
+      <title>Casaub0n Page</title>
+    </Helmet>
     <Header name='Casaub0n' />
     <MySkill />
     <GlobalStyle />

31c5624e275ff1de85b696cf8cf6c2f019d01b96
Casaubon <netzach.yoshida@gmail.com>
2020-01-13 23:31:03 +1300

    change create react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import { MainAreaConnected } from './containers/MainAreaContainer'
-import { store } from './store'
+import React from "react";
+import { Provider } from "react-redux";
+import { MainAreaConnected } from "./containers/MainAreaContainer";
+import { store } from "./store";
 
-const App = () => {
+const App: React.SFC = () => {
   return (
     <Provider store={store}>
       <MainAreaConnected />
     </Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
-import { createStore } from 'redux'
+import { createStore } from "redux";
 
-const store = createStore(() => ({}))
+const store = createStore(() => ({}));
 
-export { store }
+export { store };

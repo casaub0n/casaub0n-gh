540b20c7ab7d7e06644acedb4b1fe2cdee9ce6c7
Casaubon <netzach.yoshida@gmail.com>
2020-01-31 01:30:14 +1300

    lint this

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Provider } from "react-redux";
 import { MainAreaConnected } from "./containers/MainAreaContainer";
 import { store } from "./store";
 
-const App: React.SFC = () => {
+const App: React.FC = () => {
   return (
     <Provider store={store}>
       <MainAreaConnected />

b0795f981ee35cd999a1a1dfcdb915fb28457a9a
Casaubon <netzach.yoshida@gmail.com>
2020-02-01 10:33:08 +1300

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Provider } from "react-redux";
 import { MainAreaConnected } from "./containers/MainAreaContainer";
 import { store } from "./store";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <MainAreaConnected />
+      <Router>
+        <Route path="/" component={MainAreaConnected} />
+      </Router>
     </Provider>
   );
 };

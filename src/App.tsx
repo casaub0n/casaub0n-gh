c6b7639527b9f4ed81755d991d8a39a49e06c5f7
Casaubon <netzach.yoshida@gmail.com>
2020-02-06 21:25:47 +1300

    change Router to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { MainAreaConnected } from "./containers/MainAreaContainer";
+import { CvConnected } from "./containers/CvContainer";
 import { store } from "./store";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Route, HashRouter, Switch } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <Router>
-        <Route path="/" component={MainAreaConnected} />
-      </Router>
+      <HashRouter>
+        <Switch>
+          <Route path="/" component={MainAreaConnected} />
+          <Route path="/cv" component={CvConnected} />
+        </Switch>
+      </HashRouter>
     </Provider>
   );
 };
